507c3555c1842fef635638a25a1453d959c71bb2
danquan <dangquanattvhouse@gmail.com>
2024-07-20 00:03:10 +0700

    do not need make.bat due to CMake

--- make.bat
+++
@@ -1,7 +0,0 @@
-set OBJS=main.cpp textures.cpp game_run.cpp menu.cpp
-set OBJ_NAME=PikaJong.exe
-set COMPILER_FLAGS=-std=c++14 -w -O2
-set LINKER_FLAGS=-lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
-set INCLUDE_DIR=-I ".\x86_64-w64-mingw32-SDL\include\SDL2" -L ".\x86_64-w64-mingw32-SDL\lib"
-
-g++ -g %OBJS% %COMPILER_FLAGS% %INCLUDE_DIR% %LINKER_FLAGS% -o %OBJ_NAME%
--- make.sh
+++
@@ -1,1 +0,0 @@
-g++ -g main.cpp textures.cpp game_run.cpp menu.cpp -std=c++14 -w -O2 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -o PikaJong
